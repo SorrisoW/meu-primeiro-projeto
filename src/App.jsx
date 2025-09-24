520ba1f0938c798685808060ff666af19820c3e7
SorrisoW <metranca.w.g@gmail.com>
2025-09-24 19:46:26 -0300

    inciado projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main className="min-h-dvh grid place-items-center bg-amber-100">
+      <h1 className="text-3xl font-bold text-slate-700">
+        Hello Tailwind + React
+      </h1>
+    </main>
+  )
+}
+
+export default App
