66a7140b11a2f1ad3c9f290b6fea6c69f19cfe2c
SorrisoW <metranca.w.g@gmail.com>
2025-09-26 19:39:03 -0300

    iniciando useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,31 @@
+import { Hello } from "./components/Hello"
+import { Card } from "./components/Card"
+import { useState } from "react"
+
 const App = () => {
+
+  const [count, setCount] = useState(0)
+
+  const CARDS = [
+    {
+      id: 0,
+      title: "Card A",
+      description: "Ola eu sou o filho do Card A 😁"
+    },
+    {
+      id: 1,
+      title: "Card B",
+      description: "Ola eu sou o filho do Card B 😊"
+    },
+    {
+      id: 2,
+      title: "Card C",
+      description: "Ola eu sou o filho do Card C 😎"
+    }
+  ]
+
+  const inc = () => setCount(c => c + 1)
+
   return (
     <main className=" bg-amber-100">
       <h1 className="text-3xl font-bold text-slate-700">
@@ -6,13 +33,24 @@ const App = () => {
       </h1>
 
       <div className="container mx-auto p-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        <div className="rounded-2xl border p-4 bg-white shadow-sm">Card A</div>
-        <div className="rounded-2xl border p-4 bg-white shadow-sm">Card B</div>
-        <div className="rounded-2xl border p-4 bg-white shadow-sm">Card C</div>
+        {CARDS.map(item => (
+          <Card key={item.id} title={item.title}>
+            <p>{item.description}</p>
+          </Card>
+        ))
+        }
+
       </div>
 
-        <a href="#" className="inline-block px-4 py-2 rounded-lg border hover:bg-slate-500 focus:ring-2 focus:ring-blue-500 transition-all">Link com foco</a>
-    </main>
+      <a href="#" className="inline-block px-4 py-2 rounded-lg border  hover:bg-slate-500 focus:ring-2 focus:ring-blue-500 transition-all">Link com foco</a>
+
+      <Hello name="Sorriso" />
+
+      <button onClick={inc} className="bg-slate-600 animate-bounce text-neutral-50 px-4 py-2 rounded-2xl cursor-pointer">
+        Count: {count}
+      </button>
+
+    </main >
   )
 }
 
---
+++ src/components/Hello/index.jsx
@@ -0,0 +1,3 @@
+export const Hello = ({ name = "Mente Brilhante" }) => {
+return <h2 className="text-xl text-center">Olá, {name} 👋</h2>;
+}
